1dfa3230d586ce98cb30cebee92f1a144f60922d
Gustavo Passos <theguspassos@gmail.com>
2020-07-10 20:46:49 -0300

    add old assets back

---
+++ Assets/Scripts/Controllers/PlayerInputHandler.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.Physics;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class PlayerInputHandler : MonoBehaviour
+    {
+        [SerializeField] private Character character;
+
+        private void Update()
+        {
+            var xInput = Input.GetAxisRaw("Horizontal");
+
+            character.SetXInput(xInput);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/RandomJumpUi.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    [RequireComponent(typeof(Slider))]
+    public class RandomJumpUi : MonoBehaviour
+    {
+        private Slider slider;
+
+        private void Awake()
+        {
+            slider = GetComponent<Slider>();
+        }
+         
+        public void SetCurrentRandom(float max)
+        {
+            slider.maxValue = max;
+        }
+
+        public void ResetValue() => slider.value = 0;
+
+        public void SetCurrentValue(float currentValue)
+        {
+            slider.value = currentValue;
+        }
+    }
+}
