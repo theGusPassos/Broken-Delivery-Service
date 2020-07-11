4da9116cfdfe7ee88fcc0a3f9b49f97b770138a4
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 00:28:03 -0300

    add obstacle when passes level

---
+++ Assets/Scripts/Events/ActivateObjectOnCollisionEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Events
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class ActivateObjectOnCollisionEvent : MonoBehaviour
+    {
+        [SerializeField] private GameObject objectToActivate;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            objectToActivate.SetActive(true);
+        }
+    }
+}
