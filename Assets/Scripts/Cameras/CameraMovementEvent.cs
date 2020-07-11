0a5d453939ddf464ff620731b448e24b8f2eb821
Gustavo Passos <theguspassos@gmail.com>
2020-07-10 21:47:21 -0300

    add camera movement

---
+++ Assets/Scripts/Cameras/CameraMovementEvent.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Cameras
+{
+    [RequireComponent(typeof(BoxCollider2D))]
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class CameraMovementEvent : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraTargetPosition;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            CameraMover.Instance.SetTarget(cameraTargetPosition);
+        }
+    }
+}
---
+++ Assets/Scripts/Cameras/CameraMover.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Cameras
+{
+    public class CameraMover : MonoBehaviour
+    {
+        public static CameraMover Instance;
+
+        [SerializeField] private float cameraSpeed;
+        [SerializeField] private float distanceToStopMoving;
+
+        private Transform target;
+        private bool moving = false;
+
+        private void Awake()
+        {
+            if (Instance != null) Destroy(gameObject);
+
+            Instance = this;
+        }
+
+        public void SetTarget(Transform target)
+        {
+            moving = true;
+            this.target = target;
+        }
+
+        private void Update()
+        {
+            if (moving)
+            {
+                var x = Mathf.Lerp(transform.position.x, target.position.x, cameraSpeed);
+                var y = Mathf.Lerp(transform.position.y, target.position.y, cameraSpeed);
+
+                transform.position = new Vector3(x, y, transform.position.z);
+
+                if (Vector2.Distance(transform.localPosition, target.localPosition) < distanceToStopMoving)
+                    moving = false;
+            }
+        }
+    }
+}
