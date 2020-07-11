ed8c79df07b4a7298b3c7d9f6ad694bb67946b72
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 00:19:55 -0300

    add spikes

---
+++ Assets/Scripts/Animations/SpikeAnimation.cs
@@ -0,0 +1,33 @@
+﻿using UnityEditorInternal;
+using UnityEngine;
+
+namespace Assets.Scripts.Animations
+{
+    public class SpikeAnimation : MonoBehaviour
+    {
+        [SerializeField] private GameObject spike;
+        [SerializeField] private float timeActivated;
+        [SerializeField] private float timeInactivated;
+
+        private float currentTimer;
+        private bool active = false;
+
+        private void Update()
+        {
+            currentTimer += Time.deltaTime;
+
+            if (active && currentTimer > timeActivated)
+            {
+                spike.SetActive(false);
+                active = false;
+                currentTimer = 0;
+            }
+            else if (!active && currentTimer > timeInactivated)
+            {
+                spike.SetActive(true);
+                active = true;
+                currentTimer = 0;
+            }
+        }
+    }
+}
