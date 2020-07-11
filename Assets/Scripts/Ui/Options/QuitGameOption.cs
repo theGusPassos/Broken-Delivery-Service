682cfda8da76a77fc685ce1e6d3a48cc44487e04
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 17:31:22 -0300

    add quit game

---
+++ Assets/Scripts/Ui/Options/QuitGameOption.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Ui.Options
+{
+    public class QuitGameOption : MonoBehaviour, IPointerDownHandler
+    {
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            Application.Quit();
+        }
+    }
+}
