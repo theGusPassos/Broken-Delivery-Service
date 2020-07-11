6926fa5fe2576e7943a870fa3f881b6d1609ea20
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 18:52:15 -0300

    make musics change

--- Assets/Scripts/Sfx/MusicSystem.cs
+++ Assets/Scripts/Sfx/MusicSystem.cs
@@ -8,7 +8,13 @@ namespace Assets.Scripts.Sfx
         public static MusicSystem Instance;
 
         [SerializeField] private AudioClip mainTheme;
-        private AudioSource musicSource;
+        [SerializeField] private AudioClip endTheme;
+        [SerializeField] private float musicSpeed;
+
+        private AudioSource[] musicSources;
+        private int current = 0;
+        private int changing = 0;
+        private bool changingMusic = false;
 
         private void Awake()
         {
@@ -19,15 +25,45 @@ namespace Assets.Scripts.Sfx
 
             DontDestroyOnLoad(gameObject);
 
-            musicSource = GetComponent<AudioSource>();
-            musicSource.clip = mainTheme;
-            musicSource.Play();
+            musicSources = GetComponents<AudioSource>();
+            musicSources[current].clip = mainTheme;
+            musicSources[current].Play();
+        }
+
+        private void Update()
+        {
+            if (changingMusic)
+            {
+                musicSources[changing].volume -= musicSpeed * Time.deltaTime;
+                musicSources[current].volume += musicSpeed * Time.deltaTime;
+
+                if (musicSources[changing].volume <= 0 && musicSources[current].volume >= 1)
+                {
+                    changingMusic = false;
+                }
+            }
         }
 
         public void PlayMusic(AudioClip music)
         {
-            musicSource.clip = music;
-            musicSource.Play();
+            musicSources[current].clip = music;
+            musicSources[current].Play();
+        }
+
+        [ContextMenu("change music")]
+        public void PlayEndTheme()
+        {
+            changing = current;
+            current = current == 0 ? 1 : 0;
+            
+            changingMusic = true;
+
+            musicSources[current].volume = 0;
+
+            if (current == 0)
+                PlayMusic(mainTheme);
+            else
+                PlayMusic(endTheme);
         }
     }
 }
