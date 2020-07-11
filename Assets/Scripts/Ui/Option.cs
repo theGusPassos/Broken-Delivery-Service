bb01bb57ee25d1bd8d20e819b2b1f75a165955d2
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 02:29:14 -0300

    add sounds to menu

---
+++ Assets/Scripts/Ui/Option.cs
@@ -0,0 +1,19 @@
+﻿using Assets.Scripts.Systems;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Assets.Scripts.Ui
+{
+    public class Option : MonoBehaviour, IPointerEnterHandler, IPointerDownHandler
+    {
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            SoundSystem.Instance.PlaySoundEffect(OptionSounds.Instance.OnHoverClip);
+        }
+
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            SoundSystem.Instance.PlaySoundEffect(OptionSounds.Instance.OnClickClip);
+        }
+    }
+}
---
+++ Assets/Scripts/Ui/OptionSounds.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Ui
+{
+    public class OptionSounds : MonoBehaviour
+    {
+        public static OptionSounds Instance;
+
+        [SerializeField] private AudioClip onHoverClip;
+        [SerializeField] private AudioClip onClickClip;
+
+        public AudioClip OnHoverClip { get => onHoverClip; }
+        public AudioClip OnClickClip { get => onClickClip; }
+
+        private void Awake()
+        {
+            if (Instance != null)
+                Destroy(gameObject);
+
+            Instance = this;
+        }
+    }
+}
