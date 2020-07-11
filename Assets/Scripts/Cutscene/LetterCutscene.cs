9982affbf9f941111acc8f422d27ee6ea18a3bc5
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 15:22:27 -0300

    add end game letter show

--- Assets/Scripts/Cutscene/LetterCutscene.cs
+++ Assets/Scripts/Cutscene/LetterCutscene.cs
@@ -6,15 +6,54 @@ namespace Assets.Scripts.Cutscene
     public class LetterCutscene : MonoBehaviour
     {
         [SerializeField] private GameObject backGround;
-        [SerializeField] private TextMeshProUGUI textMesh;
+        [SerializeField] private GameObject textObject;
+        [SerializeField] private GameObject continueButton;
+
+        [SerializeField] private TextMeshProUGUI[] textsMesh;
+        [SerializeField] private float timeBetweenTexts;
+
+        private float currentTimer;
+        private int currentText = 0;
+
+        private bool active = false;
+
+        private void Awake()
+        {
+            foreach (var mesh in textsMesh)
+            {
+                mesh.enabled = false;
+            }
+        }
 
         [ContextMenu("Start Letter Cutscene")]
         public void StartLetterCutscene()
         {
             backGround.SetActive(true);
-            textMesh.gameObject.SetActive(true);
+            textObject.SetActive(true);
 
-            textMesh.text = "ending game";
+            active = true;
+        }
+
+        private void Update()
+        {
+            if (!active) return;
+
+            currentTimer += Time.deltaTime;
+
+            if (currentTimer > timeBetweenTexts)
+            {
+                if (currentText >= textsMesh.Length)
+                {
+                    continueButton.SetActive(true);
+                    active = false;
+                }
+                else
+                {
+                    textsMesh[currentText].enabled = true;
+                    currentText++;
+                    currentTimer = 0;
+                }
+            }
         }
     }
 }
