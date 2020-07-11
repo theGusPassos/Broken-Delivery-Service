abd58930131acb9a769d7bb2ca2296423e285863
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 18:22:14 -0300

    add first scenario

--- Assets/Scripts/Ui/Options/QuitGameOption.cs
+++ Assets/Scripts/Ui/Options/QuitGameOption.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace Assets.Scripts.Ui.Options
@@ -7,6 +8,13 @@ namespace Assets.Scripts.Ui.Options
     {
         public void OnPointerDown(PointerEventData eventData)
         {
+            StartCoroutine(QuitGame());
+        }
+
+        private IEnumerator QuitGame()
+        {
+            yield return new WaitForSeconds(0.5f);
+
             Application.Quit();
         }
     }
