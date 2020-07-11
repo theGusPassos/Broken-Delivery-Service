edc5d0059b32efd5c1d4991448b1adf1caf6c4f6
Gustavo Passos <theguspassos@gmail.com>
2020-07-10 22:49:31 -0300

    add sound system

---
+++ Assets/Scripts/Systems/SoundSystem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class SoundSystem : MonoBehaviour
+    {
+        [SerializeField] private AudioClip mainTheme;
+        [SerializeField] private AudioSource audioSource;
+
+        private void Awake()
+        {
+            audioSource.PlayOneShot(mainTheme);
+        }
+
+    }
+}
