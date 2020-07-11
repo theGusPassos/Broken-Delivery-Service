7d1cb964a8bbb94fe17f97d1522b7199056044df
Gustavo Passos <theguspassos@gmail.com>
2020-07-10 23:30:37 -0300

    add main menu

---
+++ Assets/Scripts/Systems/GameStarterSystem.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts.Systems
+{
+    public class GameStarterSystem : MonoBehaviour
+    {
+        [SerializeField] private string gameSceneName;
+
+        private void Update()
+        {
+            if (Input.anyKeyDown)
+            {
+                StartCoroutine(LoadScene());
+            }
+        }
+
+        private IEnumerator LoadScene()
+        {
+            AsyncOperation loading = SceneManager.LoadSceneAsync(gameSceneName);
+
+            while (!loading.isDone)
+            {
+                Debug.Log("loading");
+                yield return null;
+            }
+        }
+    }
+}
