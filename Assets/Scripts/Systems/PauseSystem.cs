a2e9822fd405fe67724804fa6ef989a13244fa03
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 15:30:34 -0300

    add pause menu

---
+++ Assets/Scripts/Systems/PauseSystem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Systems
+{
+    public class PauseSystem : MonoBehaviour
+    {
+        [SerializeField] private GameObject pauseGameCanvas;
+        private bool paused = false;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                paused = !paused;
+                pauseGameCanvas.SetActive(paused);
+
+                if (paused)
+                {
+                    Time.timeScale = 0;
+                }
+                else
+                {
+                    Time.timeScale = 1;
+                }
+            }
+        }
+    }
+}
