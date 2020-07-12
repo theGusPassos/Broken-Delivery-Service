e2c1eba029c0a1d18e9b52ee6d06884d2f937eb5
Gustavo Passos <theguspassos@gmail.com>
2020-07-11 23:04:51 -0300

    add parallax

---
+++ Assets/Scripts/Cameras/Parallax.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Camera
+{
+    public class Parallax : MonoBehaviour
+    {
+        [SerializeField] private Transform cameraTransform;
+        [SerializeField] private ParallaxLayer[] parallaxLayer;
+
+        private float lastCameraX;
+
+        private void Awake()
+        {
+            lastCameraX = cameraTransform.position.x;
+        }
+
+        private void LateUpdate()
+        {
+            float cameraDeltaX = cameraTransform.position.x - lastCameraX;
+
+            foreach (var parallax in parallaxLayer)
+            {
+                parallax.layer.position += Vector3.right * (cameraDeltaX * parallax.layerSpeed);
+            }
+
+            lastCameraX = cameraTransform.position.x;
+        }
+    }
+    
+    [System.Serializable]
+    public struct ParallaxLayer
+    {
+        public Transform layer;
+        public float layerSpeed;
+    }
+}
