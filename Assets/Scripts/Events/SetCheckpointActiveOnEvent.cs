2da085cdcfc07448914f78d79b78e066616f8740
Gustavo Passos <theguspassos@gmail.com>
2020-07-12 02:38:45 -0300

    add checkpoints

---
+++ Assets/Scripts/Events/SetCheckpointActiveOnEvent.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Events
+{
+    public class SetCheckpointActiveOnEvent : MonoBehaviour
+    {
+        [SerializeField] private Animator animator;
+        private bool used = false;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (used) return;
+
+            used = true;
+            animator.Play("checkpoint-on");
+        }
+    }
+}
