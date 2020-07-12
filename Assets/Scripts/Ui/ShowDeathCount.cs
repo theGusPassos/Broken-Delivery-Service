0c0df6bb8f72b9899a621bab3aa15bdb77b56c5c
Gustavo Passos <theguspassos@gmail.com>
2020-07-12 03:24:53 -0300

    add death counter

---
+++ Assets/Scripts/Ui/ShowDeathCount.cs
@@ -0,0 +1,14 @@
+﻿using Assets.Scripts.Systems;
+using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Ui
+{
+    public class ShowDeathCount : MonoBehaviour
+    {
+        private void Awake()
+        {
+            GetComponent<TextMeshProUGUI>().text = $"You've died {DeathCounter.numberOfDeaths} times";
+        }
+    }
+}
