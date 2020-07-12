51d8be7b5a8ee30c0e5a346dd52d4105324af7a4
Gustavo Passos <theguspassos@gmail.com>
2020-07-12 03:02:11 -0300

    add start menu

---
+++ Assets/Scripts/Controllers/DummyController.cs
@@ -0,0 +1,27 @@
+﻿using Assets.Scripts.Physics;
+using System.Linq.Expressions;
+using UnityEngine;
+
+namespace Assets.Scripts.Controllers
+{
+    public class DummyController : MonoBehaviour
+    {
+        private int direction = 1;
+        [SerializeField] private float timeToChangeDirection;
+        private float time;
+
+        [SerializeField] private Character character;
+
+        private void Update()
+        {
+            character.SetXInput(direction);
+            time += Time.deltaTime;
+
+            if (time > timeToChangeDirection)
+            {
+                direction = -direction;
+                time = 0;
+            }
+        }
+    }
+}
