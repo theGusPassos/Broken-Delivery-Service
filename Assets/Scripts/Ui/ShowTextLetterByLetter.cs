23d7723f5f9e54bfd1bc4d955e1e06dff954332f
Gustavo Passos <theguspassos@gmail.com>
2020-07-12 13:35:10 -0300

    add logo

---
+++ Assets/Scripts/Ui/ShowTextLetterByLetter.cs
@@ -0,0 +1,41 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Assets.Scripts.Ui
+{
+    public class ShowTextLetterByLetter : MonoBehaviour
+    {
+        private TextMeshProUGUI textMesh;
+        private string textToShow;
+
+        [SerializeField] private float timeToShowLetter;
+        private float currentTime;
+        private int currentLetter = 0;
+        private bool active = true;
+
+        private void Awake()
+        {
+            textMesh = GetComponent<TextMeshProUGUI>();
+
+            textToShow = textMesh.text;
+            textMesh.text = "";
+        }
+
+        private void Update()
+        {
+            if (!active) return;
+
+            currentTime += Time.deltaTime;
+
+            if (currentTime > timeToShowLetter)
+            {
+                currentTime = 0;
+                textMesh.text += textToShow[currentLetter];
+                currentLetter++;
+
+                if (currentLetter >= textToShow.Length)
+                    active = false;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Utilities/MoveToDirection.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Utilities
+{
+    public class MoveToDirection : MonoBehaviour
+    {
+        [SerializeField] private float speed;
+
+        private void Update()
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0));
+        }
+    }
+}
